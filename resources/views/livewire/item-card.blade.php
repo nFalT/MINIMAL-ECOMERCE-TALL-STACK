9d7a09b3f2196ac19462446cfc5e3374910a61f5
xGfrost <fadelakbar528@gmail.com>
2025-05-15 11:11:20 +0700

    add login and logut in welcome page

--- resources/views/livewire/item-card.blade.php
+++ resources/views/livewire/item-card.blade.php
@@ -5,10 +5,11 @@
                 width="400px">
         </div>
         <div>
-            <h2 class="line-clamp-2 px-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorum nisi, odio totam
+            <h2 class="line-clamp-2 px-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dolorum nisi,
+                odio totam
                 eum earum consectetur cum perspiciatis commodi, ipsam repellat asperiores eaque, autem harum blanditiis
                 corrupti neque! Sed, laudantium!</h2>
-            <div class="flex justify-between px-3">
+            <div class="flex justify-between px-3 py-2">
                 <div class="bg-green-200 p-1 rounded-md">
                     <h2 class="text-1xl">Outfit</h2>
                 </div>
@@ -17,14 +18,15 @@
                 </div>
             </div>
         </div>
-        <div class="flex gap-2 justify-center w-full rounded bg-blue-600 px-12 py-2 text-sm font-medium text-white text-center shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto">    
+        <div
+            class="flex gap-2 justify-center w-full rounded bg-blue-600 px-12 py-2 text-sm font-medium text-white text-center shadow hover:bg-blue-700 focus:outline-none focus:ring active:bg-blue-500 sm:w-auto">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round"
                     d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
             </svg>
-                Add to Cart
-            </a>
-        </div>
+            Add to Cart
     </a>
+</div>
+</a>
 </div>
