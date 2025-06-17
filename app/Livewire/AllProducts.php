04b78ebb1144bfcb4995a0ad985d4d653ed76913
Aswin-art <aswinarung2@gmail.com>
2025-06-18 00:24:04 +0700

    fix: product listing

--- app/Livewire/AllProducts.php
+++ app/Livewire/AllProducts.php
@@ -2,10 +2,30 @@
 
 namespace App\Livewire;
 
+use App\Models\Category;
+use App\Models\Product;
+use Illuminate\Http\Request;
 use Livewire\Component;
 
 class AllProducts extends Component
 {
+    public $products;
+    public $categoryName = "All Products";
+    public $categoryFilter;
+
+    public function mount(Request $request)
+    {
+        $this->categoryFilter = $request->input('category');
+
+        if ($this->categoryFilter) {
+            $category = Category::find($this->categoryFilter);
+            $this->categoryName = $category ? $category->name : "All Products";
+            $this->products = Product::where('category_id', $this->categoryFilter)->get();
+        } else {
+            $this->products = Product::all();
+        }
+    }
+    
     public function render()
     {
         return view('livewire.all-products');
--- app/Livewire/ProductListing.php
+++ app/Livewire/ProductListing.php
@@ -12,26 +12,28 @@ class ProductListing extends Component
     public $products = [];
     public $limit = 4;
 
+    protected $queryString = ['category_id'];
+
     public function mount()
     {
         $query = Product::query()->latest();
-        
-        if ($this->category_id > 0) {
+
+        if ($this->category_id !== 'all' && $this->category_id > 0) {
             $query->where('category_id', $this->category_id);
         }
-        
+
         if ($this->current_product_id > 0) {
             $query->where('id', '!=', $this->current_product_id);
         }
-        
-        $this->products = $query->take($this->limit)->get();
+
+        $this->products = $query->get();
     }
 
     public function addToCart($productId)
     {
         $this->dispatch('add-to-cart', productId: $productId);
     }
-    
+
     public function render()
     {
         return view('livewire.product-listing');
